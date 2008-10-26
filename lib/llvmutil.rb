3e05468fcffde11fee47d35c7345e86f021b65ce
U-KOZUE\kozue <m-72@tf6.so-net.ne.jp>
2008-10-26 10:00:28 +0900

    setdynamic/getdynamic added

---
+++ lib/llvmutil.rb
@@ -0,0 +1,51 @@
+module YARV2LLVM
+module LLVMUtil
+  def get_or_create_block(ln, b, context)
+    if context.blocks[ln] then
+      context.blocks[ln]
+    else
+      context.blocks[ln] = context.builder.create_block
+    end
+  end
+  
+  def check_same_type_2arg_static(p1, p2)
+    p1[0].add_same_type(p2[0])
+    p2[0].add_same_type(p1[0])
+  end
+  
+  def check_same_type_2arg_gencode(b, context, p1, p2)
+    if p1[0].type == nil then
+      if p2[0].type == nil then
+        print "ambious type #{p2[1].call(b, context).org}\n"
+      else
+        p1[0].type = p2[0].type
+      end
+    else
+      if p2[0].type and p1[0].type != p2[0].type then
+        print "diff type #{p1[1].call(b, context).org}\n"
+      else
+        p2[0].type = p1[0].type
+      end
+    end
+  end
+
+  def gen_common_opt_2arg(b, context, s1, s2)
+    check_same_type_2arg_gencode(b, context, s1, s2)
+    context = s1[1].call(b, context)
+    s1val = context.rc
+    #        pppp s1[0]
+    context = s2[1].call(b, context)
+    s2val = context.rc
+
+    [s1val, s2val, context]
+  end
+
+  def make_frame_struct(local)
+    member = []
+    local.each do |ele|
+      member.push ele[:type].llvm
+    end
+    Type.struct(member)
+  end
+end
+end
