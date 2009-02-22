d26715841df676b5f70637dc2fbbd4b3db986aab
U-KOZUE\kozue <m-72@tf6.so-net.ne.jp>
2009-02-22 11:53:58 +0900

    Spy meet commet

--- y2llib/spy.rb
+++ y2llib/spy.rb
@@ -7,24 +7,83 @@ class SpyServer
 
   # Top service
   class SpyServletTop<HTTPServlet::AbstractServlet
+HTML_TEMPLATE = <<EOS
+<html>
+  <style type="text/css">
+  </style>
+
+  <script type="text/javascript" id="main">
+    window.onload = function() {
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.open("GET", "/spy/update.js", true);
+      xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState == 4) {
+          eval(xmlhttp.responseText);
+        }
+      }
+      xmlhttp.send(null);
+    };
+
+  </script>
+  <body>
+    <table border>
+       <%= @table_prototye %>
+    </table>
+  </body>
+</html>
+
+EOS
+
     def initialize(sv, opt)
       super
+      table_prototye = ""
+      30.times do |i|
+        table_prototye += "<tr>"
+        3.times do |j|
+          table_prototye += "<td> <div id=\"tab#{i}-#{j}\"> </div></td>"
+        end
+        table_prototye += "</tr>"
+      end
+      @table_prototye = table_prototye
+      @page = ERB.new(HTML_TEMPLATE).result(binding)
     end
 
     def do_GET(req, res)
-      rc = "<html><body>" 
-      rc += "<table border>"
+      res.body = @page
+      res['Content-Type'] = "text/html"
+    end
+  end
+  
+  class SpyServletUpdate<HTTPServlet::AbstractServlet
+    SCRIPT_EPILOGUE = <<EOS
+    xmlhttp.open("GET", "/spy/update.js", true);
+    xmlhttp.onreadystatechange=function() {
+       if (xmlhttp.readyState == 4) {
+            eval(xmlhttp.responseText);
+       }
+    }
+    xmlhttp.send(null);
+EOS
+    def do_GET(req, res)
+      sleep(1)
+      script = ""
+
       if $pos then
+        i = 0
         $pos.each do |key, value|
-          rc += "<tr>"
-          info = YARV2LLVM::TRACE_INFO[value]
-          rc += "<td> #{info[1][0]}</td><td> #{info[1][1]}</td><td> #{info[1][3]}</td>"
-          rc += "</tr>"
+          info = YARV2LLVM::TRACE_INFO[value][1]
+          dest = "document.getElementById(\"tab#{i}-0\").innerHTML"
+          script += "#{dest} =  \"#{info[0]}\";\n"
+          dest = "document.getElementById(\"tab#{i}-1\").innerHTML"
+          script += "#{dest} =  \"#{info[1]}\";\n"
+          dest = "document.getElementById(\"tab#{i}-2\").innerHTML"
+          script += "#{dest} =  \"#{info[3]}\";\n"
+          i = i + 1
         end
       end
-      rc += "</body> </html>"
-      res.body = rc
-      res['Content-Type'] = "text/html"
+
+      res.body = script + SCRIPT_EPILOGUE
+      res['Content-Type'] = "text/javascript"
     end
   end
 
@@ -36,13 +95,13 @@ class SpyServer
 
   def initialize
     @log = DummyLog.new
-    @server = HTTPServer.new(:Port => 80, 
+    @server = HTTPServer.new(:Port => 8088, 
                              :BindAddress => "localhost",
                              :Logger => @log,
                              :AccessLog => [])
     trap("INT"){@server.shutdown}
+    @server.mount("/spy/update.js", SpyServletUpdate, nil)
     @server.mount("/spy", SpyServletTop, nil)
-    @server.mount("/spy/update", SpyServletTop, nil)
     @server.start
   end
 end
