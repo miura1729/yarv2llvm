f5b2ff38c96f5f7bbaed14050773427826a3a227
miura1729 <hideki@miura.miurakanko>
2009-06-12 22:54:20 +0900

    some bugs fixed

--- y2llib/sdl.rb
+++ y2llib/sdl.rb
@@ -2,19 +2,19 @@ require 'yarv2llvm'
 require 'sdl'
 
 module YARV2LLVM
-  MethodDefinition::RubyMethod[:open][:"SDL:Screen"] = {
+  MethodDefinition::RubyMethod[:open][:"SDL::Screen"] = {
     :argtype => [RubyType.fixnum, RubyType.fixnum, RubyType.fixnum, RubyType.fixnum],
     :rettype => RubyType.value(nil, "Return type of Screen#open", ::SDL::Screen),
   }
 
-  MethodDefinition::RubyMethod[:load_bmp][:"SDL:Surfece"] = {
+  MethodDefinition::RubyMethod[:load_bmp][:"SDL::Surface"] = {
     :argtype => [RubyType.string],
     :rettype => RubyType.value(nil, "Return type of Screen#open", ::SDL::Surface),
   }
 
 
 
-  MethodDefinition::RubyMethod[:poll][:"SDL:Event2"] = {
+  MethodDefinition::RubyMethod[:poll][:"SDL::Event"] = {
     :argtype => [RubyType.string],
     :rettype => RubyType.value(nil, "Return type of Event2#poll", ::SDL::Event2),
   }
